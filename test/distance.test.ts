202875c9075f7ce7c62974cefd1cb568263c25cb
Jack Baron <jackmbaron@gmail.com>
2019-05-27 07:22:20 +0100

    fully cover distance.ts

--- test/distance.test.ts
+++ test/distance.test.ts
@@ -5,3 +5,14 @@ test('calculates string distances', t => {
   const d = distance('distance', 'distacne')
   t.is(d, 2)
 })
+
+test('works on zero-length strings', t => {
+  const str = 'distance'
+
+  const d1 = distance(str, '')
+  const d2 = distance('', str)
+
+  t.assert(d1 === str.length)
+  t.assert(d2 === str.length)
+  t.assert(d1 === d2)
+})
